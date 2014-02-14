d4ea6e52281c36c59e8c52a3e2f0203f99280fa7
Juri Saltbacka <juri.saltbacka@gmail.com>
2014-02-14 14:39:54 +0200

    Initial

--- index.php
+++ index.php
@@ -2,13 +2,16 @@
 
   $config = json_decode(@file_get_contents('config.json'));
 
-  shell_exec('cd ' . $config->directory);
-  if(file_exists($config->directory . ".git")) {
-    shell_exec('git clone ' . $config->repository . ' .');
-  }
-  else {
-    shell_exec('git pull');
-  }
+  chdir($config->directory);
+  $commands = array(
+    file_exists($config->directory . ".git") ? "git pull" : "git clone --depth=1 --branch $config->branch $config->repository $config->directory",
+    "git submodule update --init --recursive"
+  );
 
-  shell_exec('git submodule sync');
-  shell_exec('git submodule update');
+  foreach ($commands as $command) {
+    echo "Running command: `$command`...\n";
+    echo shell_exec($command) . "\n";
+  }
+  echo "Done.";
+
+  exit;
