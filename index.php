21548e9f26495ede2c6d7d0213ca1483392a8881
Juri Saltbacka <juri.saltbacka@gmail.com>
2014-02-14 13:54:19 +0200

    Added basic functionality

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 
   $config = json_decode(@file_get_contents('config.json'));
 
-  $log = file_get_contents('post-receive.log');
-  $log .= print_r(@getallheaders(), true) . "\n" . @file_get_contents('php://input') . "\n==========\n";
-  file_put_contents('post-receive.log', $log);
+  shell_exec('cd ' . $config->directory);
+  if(file_exists($config->directory . ".git")) {
+    shell_exec('git clone ' . $config->repository . ' .');
+  }
+  else {
+    shell_exec('git pull');
+  }
+
+  shell_exec('git submodule sync');
+  shell_exec('git submodule update');
